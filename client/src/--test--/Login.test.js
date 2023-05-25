3f76305e9a4f4e17b6cbc55cad6a4bf54b2471fd
tsubo-6 <masamasamasa06100610@yahoo.co.jp>
2023-05-25 17:37:19 +0900

    Read Me記述

---
+++ client/src/--test--/Login.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {render, screen, fireEvent } from '@testing-library/react';
+import Login from '../pages/Login';
+
+describe('Login Component', ()=>{
+  test('ログインフォームが表示されること', () =>{
+    render(<Login />);
+
+    // フォーム要素の存在を確認
+    const userNameInput = screen.getByPlaceholderText('ユーザ名');
+    const emailInput = screen.getByPlaceholderText('メールアドレス');
+    const passwordInput = screen.getByPlaceholderText('パスワード');
+    const loginButton = screen.getByText('ログイン');
+
+    expect(userNameInput).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(loginButton).toBeInTheDocument();
+  });
+
+  // test('ログインボタンがクリックされた時にhandleSubmit関数が呼び出されること', () => {
+  //   const handleSubmit = jest.fn();
+  //   render(<Login handleSubmit={handleSubmit} />);
+
+  //   const loginButton = screen.getByText('ログイン');
+
+  //   fireEvent.click(loginButton);
+
+  //   expect(handleSubmit).toHaveBeenCalledTimes(1);
+  // });
+});

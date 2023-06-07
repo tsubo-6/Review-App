27ea4afddf77636b61801a8681869f214d1bee37
tsubo-6 <masamasamasa06100610@yahoo.co.jp>
2023-06-07 12:55:27 +0900

    微修正

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ const uploadRoute = require("./routes/upload.js");
 
 //expressモジュールを実体化して、定数appに代入
 const app=express()
+// helmet実装
+const helmet = require('helmet')
 const mongoose = require("mongoose");
 // require("dotenv").config();
 const path = require("path")
@@ -32,6 +34,7 @@ mongoose.connect("mongodb+srv://tsubo:Masaki61@cluster0.lmi4zi6.mongodb.net/revi
 });
 
 //ミドルウェアとして設定
+app.use(helmet())
 app.use(cors())
 app.use("/uploads", express.static(path.join(__dirname, "public/uploads")))
 app.use(express.json());
